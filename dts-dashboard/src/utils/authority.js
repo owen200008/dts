301da5c185cc909020ffc48c1f22e3bdd7e69f28
shijntao <shijntao@vaasplus.com>
2021-11-03 14:44:18 +0800

    setAuthority -> sessionStorage

--- dts-dashboard/src/utils/authority.js
+++ dts-dashboard/src/utils/authority.js
@@ -2,10 +2,10 @@
 export function getAuthority() {
   // return localStorage.getItem('antd-pro-authority') || ['admin', 'user'];
   // return sessionStorage.getItem('antd-pro-authority') || '';
-  return localStorage.getItem('antd-pro-authority') || '';
+  return sessionStorage.getItem('antd-pro-authority') || '';
 }
 
 export function setAuthority(authority) {
   // return sessionStorage.setItem('antd-pro-authority', authority);
-  return localStorage.setItem('antd-pro-authority', authority);
+  return sessionStorage.setItem('antd-pro-authority', authority);
 }
